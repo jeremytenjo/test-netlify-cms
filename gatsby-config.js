ca9088c0139aaf5685b7007ee2c6c8d4a8e0c929
Jeremy <tenjojeremy@gmail.com>
2019-06-13 21:36:45 -0700

    added cadded projects content type

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `Netlify CMS - Jer`,
+    author: `Jer`,
+    description: `A starter blog demonstrating what Gatsby can do with Netlify CMS.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `jer`,
     },
   },
   plugins: [
